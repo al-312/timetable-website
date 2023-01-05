53a2b416495625884f9dd3a389ec58fe60260083
NIBU <learnnewnibu@gmail.com>
2023-01-05 21:18:25 +0530

    firebase added

---
+++ source2.js
@@ -0,0 +1,13 @@
+document
+  .querySelector(".grid-container")
+  .appendChild(document.createElement("div"))
+  .classList.add("grid-container");
+//   <div class="grid-container">
+//   <div class="table-row">
+//     <div class="grid-item">Monday</div>
+//     <div class="grid-item">Tuesday</div>
+//     <div class="grid-item">Wednesday</div>
+//     <div class="grid-item">Thursday</div>
+//     <div class="grid-item">Friday</div>
+//   </div> -->
+// </div>
